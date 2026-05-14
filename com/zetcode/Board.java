1adf7fe9b608d22adcba4283ec7633ed45905e98
ankur2710 <ankurghosh271005@gmail.com>
2026-05-14 23:10:32 +0530

    Implement Board class for Tetris game

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -1,1 +1,83 @@
+package com.zetcode;
 
+import com.zetcode.Shape.Tetrominoe;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Board extends JPanel {
+
+    private final int BOARD_WIDTH = 10;
+    private final int BOARD_HEIGHT = 22;
+    private final int PERIOD_INTERVAL = 300;
+
+    private Timer timer;
+    private boolean isFallingFinished = false;
+    private boolean isPaused = false;
+    private int numLinesRemoved = 0;
+    private int curX = 0;
+    private int curY = 0;
+    private JLabel statusbar;
+    private Shape curPiece;
+    private Tetrominoe[] board;
+
+    public Board(Tetris parent) {
+
+        initBoard(parent);
+    }
+
+    private void initBoard(Tetris parent) {
+
+        setFocusable(true);
+        statusbar = parent.getStatusBar();
+        addKeyListener(new TAdapter());
+    }
+
+    private int squareWidth() {
+
+        return (int) getSize().getWidth() / BOARD_WIDTH;
+    }
+
+    private int squareHeight() {
+
+        return (int) getSize().getHeight() / BOARD_HEIGHT;
+    }
+
+    private Tetrominoe shapeAt(int x, int y) {
+
+        return board[(y * BOARD_WIDTH) + x];
+    }
+
+    void start() {
+
+        curPiece = new Shape();
+        board = new Tetrominoe[BOARD_WIDTH * BOARD_HEIGHT];
+
+        clearBoard();
+        newPiece();
+
+        timer = new Timer(PERIOD_INTERVAL, new GameCycle());
+        timer.start();
+    }
+
+    private void pause() {
+
+        isPaused = !isPaused;
+
+        if (isPaused) {
+
+            statusbar.setText("paused");
+        } else {
+
+            statusbar.setText(String.valueOf(numLinesRemoved));
+        }
+
+        repaint();
+    }
