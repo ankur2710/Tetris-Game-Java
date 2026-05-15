1aa9233c8b80c097fa27b85e1296de54e6b851a9
ankur2710 <ankurghosh271005@gmail.com>
2026-05-15 15:14:50 +0530

    Update Board.java

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -151,3 +151,19 @@ private void dropDown() {
             board[i] = Tetrominoe.NoShape;
         }
     }
+  private void pieceDropped() {
+
+        for (int i = 0; i < 4; i++) {
+
+            int x = curX + curPiece.x(i);
+            int y = curY - curPiece.y(i);
+            board[(y * BOARD_WIDTH) + x] = curPiece.getShape();
+        }
+
+        removeFullLines();
+
+        if (!isFallingFinished) {
+
+            newPiece();
+        }
+    }
