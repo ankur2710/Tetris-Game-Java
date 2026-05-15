3d972d24683bcea26e68636b2ceceb9e7bb6e8a8
ankur2710 <ankurghosh271005@gmail.com>
2026-05-15 15:15:21 +0530

    Update Board.java

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -167,3 +167,18 @@ private void dropDown() {
             newPiece();
         }
     }
+ private void newPiece() {
+
+        curPiece.setRandomShape();
+        curX = BOARD_WIDTH / 2 + 1;
+        curY = BOARD_HEIGHT - 1 + curPiece.minY();
+
+        if (!tryMove(curPiece, curX, curY)) {
+
+            curPiece.setShape(Tetrominoe.NoShape);
+            timer.stop();
+
+            var msg = String.format("Game over. Score: %d", numLinesRemoved);
+            statusbar.setText(msg);
+        }
+    }
