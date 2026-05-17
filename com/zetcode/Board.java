4ba8fee07f101cdc2691026d0afc263ff326b0aa
ankur2710 <ankurghosh271005@gmail.com>
2026-05-17 17:28:30 +0530

    Update Board.java

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -251,3 +251,18 @@ private void dropDown() {
                 new Color(204, 204, 102), new Color(204, 102, 204),
                 new Color(102, 204, 204), new Color(218, 170, 0)
         };
+            var color = colors[shape.ordinal()];
+
+        g.setColor(color);
+        g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
+
+        g.setColor(color.brighter());
+        g.drawLine(x, y + squareHeight() - 1, x, y);
+        g.drawLine(x, y, x + squareWidth() - 1, y);
+
+        g.setColor(color.darker());
+        g.drawLine(x + 1, y + squareHeight() - 1,
+                x + squareWidth() - 1, y + squareHeight() - 1);
+        g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1,
+                x + squareWidth() - 1, y + 1);
+    }
