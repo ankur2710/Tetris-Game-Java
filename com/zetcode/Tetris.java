897d96ce2129c10cc731408ef2c20aab8a41e417
ankur2710 <ankurghosh271005@gmail.com>
2026-05-23 13:51:05 +0530

    Update Tetris.java

--- com/zetcode/Tetris.java
+++ com/zetcode/Tetris.java
@@ -4,12 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 /*
 Java Tetris game clone
 
 Author: Jan Bodnar
 Website: https://zetcode.com
  */
+
 public class Tetris extends JFrame {
 
     private JLabel statusbar;
