a93a016d1d3ef2990d5cf062fa02b00d820d66ff
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-06 23:32:58 +0530

    Update Board.java

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -1,1 +1,30 @@
+package com.zetcode;
+
+import com.zetcode.Shape.Tetrominoe;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Board extends JPanel {
+
+    private final int BOARD_WIDTH = 10;
+    private final int BOARD_HEIGHT = 22;
+    private final int PERIOD_INTERVAL = 300;
+
+    private Timer timer;
+    private boolean isFallingFinished = false;
+    private boolean isPaused = false;
+    private int numLinesRemoved = 0;
+    private int curX = 0;
+    private int curY = 0;
+    private JLabel statusbar;
+    private Shape curPiece;
+    private Tetrominoe[] board;
 
