3652923df5299899f1d74e58da7f24aaf7a3f91a
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-07 23:13:09 +0530

    Update Board.java

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -144,6 +144,13 @@ public class Board extends JPanel {
             pieceDropped();
         }
     }
+    private void clearBoard() {
+
+        for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++) {
+
+            board[i] = Tetrominoe.NoShape;
+        }
+    }
 
 
 
