d7707df5a80ce79bbe06073834ba92e85df0a3c6
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-09 23:34:29 +0530

    Update Board.java

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -188,6 +188,12 @@ public class Board extends JPanel {
             int x = newX + newPiece.x(i);
             int y = newY - newPiece.y(i);
 
+             if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT) {
+
+                return false;
+            }
+
+
 
 
 
