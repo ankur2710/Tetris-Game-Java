e944d2181846722373c6b60359507013da93d69e
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-11 22:50:24 +0530

    Update Board.java

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -241,6 +241,13 @@ public class Board extends JPanel {
             curPiece.setShape(Tetrominoe.NoShape);
         }
     }
+     private void drawSquare(Graphics g, int x, int y, Tetrominoe shape) {
+
+        Color colors[] = {new Color(0, 0, 0), new Color(204, 102, 102),
+                new Color(102, 204, 102), new Color(102, 102, 204),
+                new Color(204, 204, 102), new Color(204, 102, 204),
+                new Color(102, 204, 204), new Color(218, 170, 0)
+        };
 
         
 
